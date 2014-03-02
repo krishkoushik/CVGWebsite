966aa5d56c57dbc81858e62682de3be678f445d7
Ramakrish <sanjaygana@gmail.com>
2014-03-02 22:40:06 +0530

    "Queue without AJAX and url resolution"

---
+++ cvg/onlinejudge/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from onlinejudge.models import CodeToCompile
+from onlinejudge.models import RequestQueue
+
+admin.site.register(CodeToCompile)
+admin.site.register(RequestQueue)
+
+
+ 
