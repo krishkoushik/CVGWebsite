3d7e91f36f79d93c822529c3b970a5fbc0d0cb2b
mohanaprasad1994 <mohanaprasad1994@yahoo.co.in>
2014-03-02 12:06:43 +0530

    Before_blog

--- cvg/cvg/urls.py
+++ cvg/cvg/urls.py
@@ -30,4 +30,9 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    
+    
 )
+
+#urlpatterns += staticfiles_urlpatterns()
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cvg/website/models.py
+++ cvg/website/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+	title=models.CharField(max_length=100)
+	image=models.ImageField(upload_to='media/projects')
+	
+	def __unicode__(self):
+		return self.title
+	
+	
+class Video(models.Model):
+	name=models.CharField(max_length=100)
+	video=models.URLField()
+	
+	def __unicode__(self):
+		return self.name
+	
+	
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    brief = models.CharField(max_length=300)
+    detail = models.CharField(max_length=1000)
+    main_imag=models.ForeignKey(Photo,related_name='main')
+    imag=models.ManyToManyField(Photo,related_name='aux')
+    videos=models.ManyToManyField(Video)
+    date = models.DateField()
+
+    def __unicode__(self):
+        return self.title
+	class meta:
+		ordering=['date']
+
+
+
+
