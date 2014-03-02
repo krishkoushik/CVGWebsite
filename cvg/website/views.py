cb57905c065e275d3ab50cc963e1bae91f38e10d
mohanaprasad1994 <mohanaprasad1994@yahoo.co.in>
2014-03-02 13:36:55 +0530

    pagination-projects

--- cvg/website/views.py
+++ cvg/website/views.py
@@ -6,6 +6,7 @@ from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 from models import Project,Photo,Video
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def home(request):
 	return render_to_response("home.html",context_instance=RequestContext(request))
 	
@@ -13,7 +14,19 @@ def about(request):
 	return render_to_response("about.html",context_instance=RequestContext(request))
 	
 def projects(request):
-	projects=Project.objects.all()
+
+	project_list=Project.objects.all()
+	paginator = Paginator(project_list, 2) # Show 25 contacts per page
+	page = request.GET.get('page')
+	try:
+		projects = paginator.page(page)
+	except PageNotAnInteger:
+		projects = paginator.page(1) # If page is not an integer, deliver first page.
+	except EmptyPage:
+		projects = paginator.page(paginator.num_pages)
+    
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        
 	return render(request,'projects.html',{'projects':projects,})
 	#return render_to_response("projects.html",context_instance=RequestContext(request))
 	
