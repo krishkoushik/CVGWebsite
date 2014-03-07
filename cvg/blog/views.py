7ff47a48b4f217401e4b8f1943b1b369989de5ef
mohanaprasad1994 <mohanaprasad1994@yahoo.co.in>
2014-03-07 15:49:25 +0530

    Working

--- cvg/blog/views.py
+++ cvg/blog/views.py
@@ -3,16 +3,25 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import redirect, render_to_response, get_object_or_404
 from django.template import RequestContext
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from models import Post
 from forms import PostForm, CommentForm
+from django.shortcuts import render
 
 #@user_passes_test(lambda u: u.is_superuser)
 
 def blog(request):
-	posts=Post.objects.all().order_by("-created_on")
+	post_list=Post.objects.all().order_by("-created_on")
+	paginator = Paginator(post_list, 1) # Show 25 contacts per page
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		posts = paginator.page(1) # If page is not an integer, deliver first page.
+	except EmptyPage:
+		posts = paginator.page(paginator.num_pages)
+	return render(request,"blog.html",{'posts':posts,})
 	
-	return render_to_response("blog.html",{'posts':posts,},context_instance=RequestContext(request))
 
 def add_post(request):
 	form = PostForm(request.POST or None)
@@ -25,7 +34,7 @@ def add_post(request):
 
 def blogpost(request, slug):
 	post = get_object_or_404(Post, slug=slug)
-	#comments=Post.comments.all()
+	
 	form = CommentForm(request.POST or None)
 	if form.is_valid():
 		comment = form.save(commit=False)
